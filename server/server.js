8625d6be4d9a15e4a4c363050f23dbbb24eeda33
Mand <mend_mzury@hotmail.com>
2018-12-07 16:22:39 +0200

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const port = process.env.PORT || 8080;
-const path = require("path")
-const publicPath = path.join(__dirname, "../public")
+const socketIO = require("socket.io")
+const http = require("http")
 const express = require("express");
-const app = express()
+const path = require("path")
+
+const publicPath = path.join(__dirname, "../public")
+const app = express();
+var server = http.createServer(app)
+var io = socketIO(server)
 
 console.log(__dirname + "/../public");
 console.log(publicPath);
@@ -13,8 +18,16 @@ app.use(express.static(publicPath))
 //   res.sendFile(publicPath + "/index.html")
 // })
 
+io.on("connection", (socket)=>{
+  console.log("new user connected ")
+
+  socket.on("disconnect", ()=>[
+    console.log("client disconnected")
+  ])
+})
 
 
-app.listen(port, function(){
+
+server.listen(port, function(){
   console.log("app is on", port)
 })
